6d011a0d8722c0c9a414c6daebed0f970eccd721
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2023-12-28 20:55:43 +0200

    Initialize ExpediaFrontend and Demonstrate Customer Object
    In the main function, instantiate the ExpediaFrontend class and run the application. Additionally, create a Customer object and briefly demonstrate its instantiation within the context of the Expedia application.

---
+++ ExpediaSystem.cpp
@@ -0,0 +1,21 @@
+#include "ExpediaFrontend.h"
+#include "CustomerUser.h"
+
+int main()
+{
+    ExpediaFrontend site;
+	site.Run();
+
+	User* user = new Customer();
+	dynamic_cast<Customer*>(user);
+
+
+
+
+
+
+
+
+
+    return 0;
+}
