aeaef819968ccb6ad34cd071ebfc6e2c01d3a1b4
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2023-12-28 20:41:59 +0200

    Implement Hotel APIs with Local Implementations
    Introduces HotelAPIs.h, providing local APIs for practicing interactions with external hotel systems. The file includes implementations for Hilton and Marriott hotels, allowing for hands-on practice with hotel room searches. These local implementations are used to demonstrate how to design a system that doesn't directly depend on APIs but interfaces with them.

---
+++ HotelApis.h
@@ -0,0 +1,61 @@
+#ifndef HOTELAPIS_H
+#define HOTELAPIS_H
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+class HiltonRoom {
+public:
+	string room_type;
+	int available;
+	double price_per_night;
+	string date_from;
+	string date_to;
+};
+
+class HiltonHotelAPI {
+public:
+	static vector<HiltonRoom> SearchRooms(string country, string city, string from_date, string to_date, int adults, int children, int needed_rooms) {
+		vector<HiltonRoom> rooms;
+
+		rooms.push_back({ "Interior View", 6, 200.0, "29-01-2022", "10-02-2022" });
+		rooms.push_back({ "City View", 3, 300.0, "29-01-2022", "10-02-2022" });
+		rooms.push_back({ "Deluxe View", 8, 500.0, "29-01-2022", "10-02-2022" });
+
+		return rooms;
+	}
+};
+
+class MarriottFoundRoom {
+public:
+	string room_type;
+	int available;
+	double price_per_night;
+	string date_from;
+	string date_to;
+};
+
+class MarriottHotelAPI {
+public:
+	static vector<MarriottFoundRoom> FindRooms(string from_date, string to_date, string country, string city, int needed_rooms, int adults, int children) {
+		vector<MarriottFoundRoom> rooms;
+
+		rooms.push_back({ "City View", 8, 320.0, "29-01-2022", "10-02-2022" });
+		rooms.push_back({ "Interior View", 8, 220.0, "29-01-2022", "10-02-2022" });
+		rooms.push_back({ "Private View", 5, 600.0, "29-01-2022", "10-02-2022" });
+
+		return rooms;
+	}
+};
+
+
+
+
+
+
+
+
+
+
+#endif
